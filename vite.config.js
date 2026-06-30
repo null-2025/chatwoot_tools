ce1a6b089574e1244cd1fe284b3e0742fef2ea4a
null-2025 <227227084+null-2025@users.noreply.github.com>
2026-06-30 11:54:04 +0630

    Setup GitHub page deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/chatwoot_tools/",
   server: {
     proxy: {
       "/chatwoot-api": {
